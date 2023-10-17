7a0b246ab254d5ae3395d26d055990f428084e49
vvk1612 <vvk1612@gmail.com>
2023-10-17 15:38:51 +0530

    First Commit

---
+++ src/GitHub/NewProg.java
@@ -0,0 +1,24 @@
+package GitHub;
+
+public class NewProg {
+
+	public static void main(String[] args) {
+		int n=5,fact=1;
+
+		for(int i=1; i<=n; i++) {
+			fact*=i;
+			{
+				System.out.println(fact);
+				
+			}
+			
+			
+		}
+			}
+
+		
+
+
+	}
+
+
